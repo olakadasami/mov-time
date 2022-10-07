5ed33b221b4c11a8c19d90322b4f65562780ede2
Samuel Olakada <olakadasamuel@gmail.com>
2022-10-07 11:30:46 +0100

    added closing navbar by clicking the page

--- components/HomeLayout.jsx
+++ components/HomeLayout.jsx
@@ -15,24 +15,25 @@ function HomeLayout({ children }) {
 
     // Home layout is the container component
     return (
-        <div className="bg-primary-dark h-screen overflow-hidden text-primary-light flex">
+        <div className="bg-primary-dark h-screen md:overflow-hidden text-primary-light flex">
             {/* Side Nav */}
-            <nav className={`md:w-3/12 xl:w-2/12 h-full lg:px-6 hidden md:block bg-secondary-dark text-secondary-light`}>
+            <nav className={`md:w-3/12 xl:w-2/12 pb-8 overflow-y-auto h-full lg:px-6 hidden md:block bg-secondary-dark text-secondary-light`}>
                 <SideNav />
             </nav>
 
             {/* Mobile Nav */}
-            <nav className={`w-[250px] h-full pt-2 absolute top-0 z-50 ${showNav ? "left-0" : "-left-full"} transition-all ease-in-out duration-300 md:hidden bg-secondary-dark text-secondary-light`}>
+            <nav className={`w-[280px] h-full pb-8 overflow-y-auto pt-2 absolute top-0 z-50 ${showNav ? "left-0" : "-left-full"} transition-all ease-in-out duration-300 md:hidden bg-secondary-dark text-secondary-light`}>
                 <SideNav />
             </nav>
 
-            <main className='h-full w-full md:w-9/12 xl:w-7/12 relative overflow-y-auto p-4 sm:px-8 py-8 md:px-6'>
+            <main onClick={showNav ? (() => setShowNav(false)) : undefined}
+                className='h-full z-10 w-full md:w-9/12 xl:w-7/12 relative overflow-y-auto p-4 sm:px-8 py-8 md:px-6'>
                 <TopNav showNav={showNav} toogleNavHandler={toogleNavHandler} />
                 {/* children refers to the individual pages/routes */}
                 {children}
             </main>
 
-            <aside className="h-full hidden xl:block xl:w-3/12 py-8 px-6 bg-secondary-dark">
+            <aside className="h-full hidden overflow-y-auto xl:block xl:w-3/12 py-8 px-6 bg-secondary-dark">
                 <RightAside />
             </aside>
         </div>
